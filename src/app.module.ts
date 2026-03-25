5cb4d0fda1e738e938e1e7d5c22539268345e032
Luciano Augusto Campagnoli da Silva <luti4497@gmail.com>
2026-03-25 18:23:51 -0300

    feature: Installs nestjs config

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,14 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { Web3serviceService } from './web3service/web3service.service';
-
+import { Web3Service } from './web3/web3service.service';
+import { ConfigModule } from '@nestjs/config';
+import { MongodbModule } from './mongodb/mongodb.module';
+import { Web3Module } from './web3/web3.module';
+  
 @Module({
-  imports: [],
+  imports: [MongodbModule, Web3Module],
   controllers: [AppController],
-  providers: [AppService, Web3serviceService],
+  providers: [AppService],
 })
 export class AppModule {}
